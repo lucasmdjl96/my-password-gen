e6c36e34e35d1d449c92ab05fdbeede0e1f01793
Lucas M. de Jong Larrarte <lucasmdjl96@gmail.com>
2022-10-22 12:47:20 +0200

    Change hash algorithms to SHA3-256 (server) and SHA-512/256 (client)

--- src/jvmMain/kotlin/com/lucasmdjl/passwordgenerator/server/crypto/Sha256.kt
+++ src/jvmMain/kotlin/com/lucasmdjl/passwordgenerator/server/crypto/Sha256.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 private fun sha256(message: String): String {
     val input = message.toByteArray()
-    val bytes = MessageDigest.getInstance("SHA-256").digest(input)
+    val bytes = MessageDigest.getInstance("SHA3-256").digest(input)
     return Base64.getUrlEncoder().encodeToString(bytes)
 }
 
