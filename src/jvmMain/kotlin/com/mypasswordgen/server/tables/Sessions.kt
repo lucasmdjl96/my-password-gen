e98857480da1af303dba898fc1dbb11cab4c9ecd
Lucas M. de Jong Larrarte <lucasmdjl96@gmail.com>
2022-11-11 22:06:14 +0100

    Add stale session removal and init script capabilities

--- src/jvmMain/kotlin/com/mypasswordgen/server/tables/Sessions.kt
+++ src/jvmMain/kotlin/com/mypasswordgen/server/tables/Sessions.kt
@@ -2,10 +2,13 @@ package com.mypasswordgen.server.tables
 
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.kotlin.datetime.CurrentDate
+import org.jetbrains.exposed.sql.kotlin.datetime.date
 
 object Sessions : UUIDTable() {
 
     var lastUserId = reference("last_user_id", Users.id, onDelete = ReferenceOption.SET_NULL).nullable()
+    var dateCreated = date("date_created").defaultExpression(CurrentDate)
 
     init {
         index(isUnique = true, lastUserId)
